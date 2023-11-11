2f6265ca6bf75a35f5bfffeef322ba6de1d3bd1f
Ulung32 <13521122@mahasiswa.itb.ac.id>
2023-11-11 23:36:16 +0700

    chore: connecting to mysql (still error), lanjutin dew

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,13 @@
 
 import javax.xml.ws.Endpoint;
 
+import repo.repository;
+
 public class Main {
     public static void main(String[] args) {
         Endpoint.publish("http://0.0.0.0:6001/ws/hello", new ws.TestWSImpl());
         System.out.println("Service berhasil di publish");
+        repository repo = new repository();
+        System.out.println(repo.getConnection());
     }
 }
