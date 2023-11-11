d68a8226811c691ceab10c489902bde1b609fd96
Ulung32 <13521122@mahasiswa.itb.ac.id>
2023-11-11 21:14:28 +0700

    chore migration database

---
+++ migration/migration.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS logging;
+DROP TABLE IF EXISTS pesanan;
+DROP TABLE IF EXISTS detail_pesanan;
+
+
+
+CREATE TABLE pesanan (
+    id INT PRIMARY KEY,
+    id_kurir INT DEFAULT 0,
+    alamat VARCHAR(255),
+    nama_penerima VARCHAR(255),
+    status ENUM('searching_courier', 'pickup', 'transit', 'delivered'),
+    keterangan TEXT
+);
+
+
+CREATE TABLE detail_pesanan (
+    id INT PRIMARY KEY,
+    id_pesanan INT,
+    nama_produk VARCHAR(255),
+    quantity INT,
+    FOREIGN KEY (id_pesanan) REFERENCES pesanan(id)
+);
+
+
